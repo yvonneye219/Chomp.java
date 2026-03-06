ab736ab0665bc97797ab07eb0292cafdb7e1281c
yvonneye <yvonne_ye26@milton.edu>
2026-03-06 12:06:00 -0500

    classwork

--- src/MyChomp.java
+++ src/MyChomp.java
@@ -3,6 +3,7 @@ public class MyChomp {
     public static void main(String[] args) {
 
         boolean[][][] lose = new boolean[4][4][4];
+        lose[1][0][0] = true;
 
         for (int c1 = 1; c1 <= 3; c1++) {
             for (int c2 = 0; c2 <= c1; c2++) {
@@ -39,20 +40,22 @@ public class MyChomp {
                             if (!(nc1 == 0 && nc2 == 0 && nc3 == 0)) {
 
 
-                                // check if move leads to a lose board
-                                if ((nc1 == 1 && nc2 == 0 && nc3 == 0) ||
-                                        (nc1 == 2 && nc2 == 1 && nc3 == 0)) {
-                                    win = true;
+                                if (!(nc1 == 0 && nc2 == 0 && nc3 == 0)) {
+                                    if (lose[nc1][nc2][nc3]) {
+                                        win = true;
+                                    }
                                 }
                             }
                         }
                     }
 
-                    if (win)
+                    if (win) {
                         System.out.println("WIN  (" + c1 + "," + c2 + "," + c3 + ")");
-                    else
+                        lose[c1][c2][c3] = false;
+                    } else {
                         System.out.println("LOSE (" + c1 + "," + c2 + "," + c3 + ")");
-
+                        lose[c1][c2][c3] = true;
+                    }
 
                     for (int col = 1; col <= 3; col++) {
 
