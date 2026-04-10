54320e96e0e8d33a19004e8718a7ed80517594d9
yvonneye <yvonne_ye26@milton.edu>
2026-04-10 12:03:32 -0400

    in progress

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -13,53 +13,77 @@ public class MyPlayer {
 
         gameBoard = pBoard;
 
-        // Count chips in each column
-        for (int c = 0; c < 3; c++) {
+        // Count alive chips in each column on the 3x3 board
+        for (int col = 0; col < 3; col++) {
             int count = 0;
-            for (int r = 0; r < 3; r++) {
-                if (gameBoard[r][c] != null) {
+            for (int row = 0; row < 3; row++) {
+                if (gameBoard[row][col].isAlive) {
                     count++;
                 }
             }
-            columns[c] = count;
+            columns[col] = count;
         }
 
-        int c1 = columns[0];
-        int c2 = columns[1];
-        int c3 = columns[2];
+        int a = columns[0];
+        int b = columns[1];
+        int c = columns[2];
 
-        // Try every possible move
-        for (int col = 1; col <= 3; col++) {
-            int height = 0;
-            if (col == 1) height = c1;
-            if (col == 2) height = c2;
-            if (col == 3) height = c3;
+        // Try reducing column 3
+        if (c > 0) {
+            for (int x = 1; x <= c; x++) {
+                int na = a;
+                int nb = b;
+                int nc = c - x;
 
-            for (int row = 1; row <= height; row++) {
-                int nc1 = c1;
-                int nc2 = c2;
-                int nc3 = c3;
+                if (na == 1 && nb == 0 && nc == 0) {
+                    System.out.println("match in col 3");
+                    return new Point(nc, 2);
+                }
+            }
+        }
+        // Try reducing column 2
+        if (b > 0) {
+            for (int x = 1; x <= b; x++) {
+                int na = a;
+                int nb = b - x;
+                int nc = c;
 
-                if (col <= 1 && nc1 >= row) nc1 = row - 1;
-                if (col <= 2 && nc2 >= row) nc2 = row - 1;
-                if (col <= 3 && nc3 >= row) nc3 = row - 1;
+                if (nb < nc) {
+                    nc = nb;
+                }
 
-                if (nc2 > nc1) nc2 = nc1;
-                if (nc3 > nc2) nc3 = nc2;
-
-                if ((nc1 == 1 && nc2 == 0 && nc3 == 0) ||
-                        (nc1 == 2 && nc2 == 1 && nc3 == 0)) {
-                    System.out.println("Winning move: col=" + (col - 1) + " row=" + (row - 1));
-                    return new Point(col - 1, row - 1);
+                if (na == 1 && nb == 0 && nc == 0) {
+                    System.out.println("match in col 2");
+                    return new Point(nb, 1);
                 }
             }
         }
 
-        // Fallback: first legal move
-        for (int c = 0; c < 3; c++) {
-            if (columns[c] > 0) {
-                System.out.println("Fallback move: col=" + c + " row=" + (columns[c] - 1));
-                return new Point(c, columns[c] - 1);
+        // Try reducing column 1
+        if (a > 0) {
+            for (int x = 1; x <= a; x++) {
+                int na = a - x;
+                int nb = b;
+                int nc = c;
+
+                if (na < nb) {
+                    nb = na;
+                }
+                if (nb < nc) {
+                    nc = nb;
+                }
+
+                if (na == 1 && nb == 0 && nc == 0) {
+                    System.out.println("match in col 1");
+                    return new Point(na, 0);
+                }
+            }
+        }
+
+        // Fallback: choose the top alive chip in the rightmost non-empty column
+        for (int col = 2; col >= 0; col--) {
+            if (columns[col] > 0) {
+                return new Point(columns[col] - 1, col);
             }
         }
 
